a8e076f2c1c7ea719a5850d2934af09e24ad35c9
Miku Suga <mikusuga@gmail.com>
2020-05-01 16:54:49 +0700

    Dividing up images

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,6 +1,6 @@
 # Image preprocessing variables
-IMG_WIDTH = 1500
-IMG_HEIGHT = 750
+IMG_WIDTH = 1000
+IMG_HEIGHT = 1000
 IMAGE_NET_MEAN_RGB = [103.939,116.779,123.68]
 CHANNELS = 3
 
@@ -12,7 +12,7 @@ LOSS_FACTOR = 1.25
 
 # Neural Network hyperparameters
 CONTENT_LAYER = 'block4_conv2'
-STYLE_LAYERS = ['block1_conv2', 'block2_conv2', 'block3_conv3', 'block4_conv3', 'block5_conv3']
+STYLE_LAYERS = ['block1_conv2', 'block2_conv2', 'block3_conv2', 'block4_conv3', 'block5_conv3']
 
 # Optimizer hyperparameters
 ITER_PER_EPOCH = 20
