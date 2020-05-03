f79bc9aed2c7066fe427a14281bce9f2a7826ceb
Desmond Cheong Zhi Xi <desmondcheongzx@gmail.com>
2020-05-03 20:20:41 +0800

    Change renormalization method

--- stitching.py
+++ stitching.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from PIL import ImageEnhance, Image, ImageFilter
 
 NUM_PASSES = 1
-FIX_WIDTH = 250
+FIX_WIDTH = 200
 FIX_FACTOR = 1/FIX_WIDTH
 IMG_WIDTH = 1500
 IMG_HEIGHT = 750
@@ -66,13 +66,11 @@ for h in range(SCALE):
                 cur_quad[-i, :, :] /= 1 + cur_factor
 
         # Renormalize Quadrant
-        norm = np.linalg.norm(cur_quad)
-        cur_quad /= norm
+        cur_quad = (cur_quad - np.mean(cur_quad))/np.std(cur_quad)
+        cur_quad = cur_quad * np.std(base_quad) + np.mean(base_quad)
     
         image[h*IMG_HEIGHT:(h+1)*IMG_HEIGHT, w*IMG_WIDTH:(w+1)*IMG_WIDTH,:] = cur_quad
 
-base_norm = np.linalg.norm(base)
-image *= base_norm
 
 # Normalize edges along bands
 for _ in range(HORIZONTAL_PASSES):
@@ -84,10 +82,14 @@ for _ in range(HORIZONTAL_PASSES):
                 cur_factor = (np.exp(1 - FIX_FACTOR * i)-1)
                 top_band = top_quad[-i, :, :]
                 bot_band = bot_quad[i-1, :, :]
-                top_norms = np.linalg.norm(top_band, axis=0)
-                bot_norms = np.linalg.norm(bot_band, axis=0)
-                top_band = (top_band * bot_norms) / top_norms
-                bot_band = (bot_band * top_norms) / bot_norms
+                top_mean = np.mean(top_band)
+                top_std = np.std(top_band)
+                bot_mean = np.mean(bot_band)
+                bot_std = np.std(bot_band)
+                top_band = (top_band - top_mean) / top_std
+                top_band = top_band * bot_std + bot_mean
+                bot_band = (bot_band - bot_mean) / bot_std
+                bot_band = bot_band * top_std + top_mean
                 top_quad[-i, :, :] += cur_factor * top_band
                 bot_quad[i-1, :, :] += cur_factor * bot_band
                 top_quad[-i, :, :] /= 1 + cur_factor
@@ -102,10 +104,14 @@ for _ in range(VERTICAL_PASSES):
                 cur_factor = (np.exp(1 - FIX_FACTOR * i)-1)
                 left_band = left_quad[:, -i, :]
                 right_band = right_quad[:, i-1, :]
-                left_norms = np.linalg.norm(left_band, axis=0)
-                right_norms = np.linalg.norm(right_band, axis=0)
-                left_band = (left_band * right_norms) / left_norms
-                right_band = (right_band * left_norms) / right_norms
+                left_mean = np.mean(left_band)
+                left_std = np.std(left_band)
+                right_mean = np.mean(right_band)
+                right_std = np.std(right_band)
+                left_band = (left_band - left_mean) / left_std
+                left_band = left_band * right_std + right_mean
+                right_band = (right_band - right_mean) / right_std
+                right_band = right_band * left_std + left_mean
                 left_quad[:, -i, :] += cur_factor * left_band
                 right_quad[:, i-1, :] += cur_factor * right_band
                 left_quad[:, -i, :] /= 1 + cur_factor
@@ -113,6 +119,7 @@ for _ in range(VERTICAL_PASSES):
 
 # Fix seam
 #image = image[:, 10:-10, :]
+'''
 image_copy = np.copy(image)
 for i in range(1, FIX_WIDTH):
     cur_factor = (np.exp(1 - FIX_FACTOR * i)-1)
@@ -120,6 +127,7 @@ for i in range(1, FIX_WIDTH):
     image[:, -i, :] += cur_factor * image_copy[:, i-1, :]
     image[:, i-1, :] /= 1 + cur_factor
     image[:, -i, :] /= 1 + cur_factor
+'''
 
 # And renormalize everything band by band
 for h in range(SCALE):
@@ -128,18 +136,19 @@ for h in range(SCALE):
         base_quad = large_base[h*IMG_HEIGHT:(h+1)*IMG_HEIGHT, w*IMG_WIDTH:(w+1)*IMG_WIDTH,:]
         for i in range(IMG_HEIGHT):
             cur_band = cur_quad[i, :, :]
-            norm = np.linalg.norm(cur_band)
+
             base_band = base_quad[i, :, :]
-            base_norm = np.linalg.norm(base_band)            
-            cur_band = (cur_band * base_norm) / norm            
+
+            cur_band = (cur_band - np.mean(cur_band)) / np.std(cur_band)
+            cur_band = cur_band * np.std(base_band) + np.mean(base_band)
             cur_quad[i, :, :] = cur_band
 
         for i in range(IMG_WIDTH):
             cur_band = cur_quad[:, i, :]
-            norm = np.linalg.norm(cur_band)
             base_band = base_quad[:, i, :]
-            base_norm = np.linalg.norm(base_band)            
-            cur_band = (cur_band * base_norm) / norm            
+            cur_band = (cur_band - np.mean(cur_band)) / np.std(cur_band)
+            cur_band = cur_band * np.std(base_band) + np.mean(base_band)
+
             cur_quad[:, i, :] = cur_band
             
         image[h*IMG_HEIGHT:(h+1)*IMG_HEIGHT, w*IMG_WIDTH:(w+1)*IMG_WIDTH,:] = cur_quad
