ba745ba3d65154ee5ebfbc53592cfd8d011856d4
Desmond Cheong Zhi Xi <desmondcheongzx@gmail.com>
2020-05-04 12:16:30 +0800

    Debug gram matrix

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def calc_style_loss(output, num_layers):
 
 # Using the gram matrix equation
 def gram_matrix(image):
-    image = backend.batch_flatten(backend.permute_dimensions(image, (2, 0, 1)))
+    image = backend.batch_flatten(backend.permute_dimensions(image, (0, 2, 1)))
     return backend.dot(image, backend.transpose(image))
     # numerator_matrix = tf.einsum('bijc,bijd->bcd', image, image)
     # image_dimension = tf.shape(image)
